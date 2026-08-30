d99976a76d79029df6ab20705c0b835509dca380
akeredolu <akeredolu.waheed@gmail.com>
2026-08-30 19:24:19 +0100

    Add sitemap for Google indexing

---
+++ main/sitemaps.py
@@ -0,0 +1,17 @@
+from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
+
+
+class StaticViewSitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.8
+
+    def items(self):
+        return [
+            "home",
+            "register",
+            "enroll_now",
+        ]
+
+    def location(self, item):
+        return reverse(item)
--- stemsite/urls.py
+++ stemsite/urls.py
@@ -1,13 +1,30 @@
 from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
 from django.urls import path, include
+from django.http import HttpResponse
+
+from main.sitemaps import StaticViewSitemap
+
+
+sitemaps = {
+    "static": StaticViewSitemap,
+}
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  # ✅ The real Django admin
+    path('favicon.ico', lambda request: HttpResponse(status=204)),
+
+    path(
+        'sitemap.xml',
+        sitemap,
+        {'sitemaps': sitemaps},
+        name='django.contrib.sitemaps.views.sitemap',
+    ),
+
+    path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('chat/', include('chat.urls')),
-    
-    path('admin/broadcast/', include('main.urls_admin')), 
+
+    path('admin/broadcast/', include('main.urls_admin')),
     path('services/', include('services.urls')),
 ]
-
